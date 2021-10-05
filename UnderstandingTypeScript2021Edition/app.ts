d9f2af84bb4185733aa650ce78175721ea60fdc5
Alberto González <algonzalez@foundationmedicine.com>
2021-10-05 13:13:04 -0500

    The enum role was added to the person object.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -1,17 +1,11 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+enum Role { ADMIN, READ_ONLY, AUTHOR}
+
+const person= {
   name: 'Maximilian',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.ADMIN
 }
-
-person.role.push('admin')
-// person.role[1] = 10
 
 let favoriteActivies: string[];
 favoriteActivies = ['Sports']
