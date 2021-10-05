9e6b79ea521dcf02b742fd482ddbbe604ca2d33b
Alberto González <algonzalez@foundationmedicine.com>
2021-10-05 16:20:50 -0500

    The proyect was initialized as an TS proyect.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -1,22 +1,4 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
-}
+const userName = 'Maximilian'
 
-function printResult(num: number) {
-  console.log('Result: ' + num);
-}
+console.log(userName);
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result)
-}
-
-let combineValues: (n1: number, n2: number) => number;
-combineValues = add;
-// combineValues = printResult
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-})
