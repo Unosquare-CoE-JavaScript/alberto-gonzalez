8a751757448395a0c3601c226a20b4a9ed6e6b95
Alberto González <algonzalez@foundationmedicine.com>
2021-10-05 12:51:33 -0500

    The array hobbies was added to the person object.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -1,17 +1,14 @@
-function add(n1:number, n2:number, showResult: boolean, phrase:string){
-  const result = n1 + n2
-  if(showResult){
-    console.log(phrase + result);
-  }
-  else{
-    return result;
-  }
+const person = {
+  name: 'Maximilian',
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
 }
 
-const number1 = 5;
-const number2 = 2;
-const printResult = true;
-const resultPhrase = 'Result is: ';
+let favoriteActivies: string[];
+favoriteActivies = ['Sports']
 
-const result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
+console.log(person.name);
+
+for(const hobby of person.hobbies){
+  console.log(hobby.toUpperCase());
+}
