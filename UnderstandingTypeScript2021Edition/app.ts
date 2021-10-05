831ad9c01b589dbb6dc55bdf2d89a41167029ee9
Alberto González <algonzalez@foundationmedicine.com>
2021-10-05 14:17:17 -0500

    An function type was defined.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -1,26 +1,13 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text'
-
-type User = { name: string; age: number };
-const u1: User = { name: 'Max', age: 30 }; // this works!
-
-function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 == 'number' || resultConversion == 'as-number') {
-    result = +input1 + +input2
-  }
-  else {
-    result = input1.toString() + input2.toString()
-  }
-  return result;
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
+function printResult(num: number) {
+  console.log('Result: ' + num);
+}
 
-const combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges);
+let combineValues: (n1: number, n2: number) => number;
+combineValues = add;
+// combineValues = printResult
 
-
-const combineNames = combine('Max', 'Anna', 'as-text')
-console.log(combineNames);
+console.log(combineValues(8, 8));
