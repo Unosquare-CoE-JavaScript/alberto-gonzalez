fba66ce486440d4bc497d8bedf1ffed1fbab5ff3
Alberto González <algonzalez@foundationmedicine.com>
2021-10-05 14:26:20 -0500

    An function was specified in the addHandle function.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -6,8 +6,17 @@ function printResult(num: number) {
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result)
+}
+
 let combineValues: (n1: number, n2: number) => number;
 combineValues = add;
 // combineValues = printResult
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+})
