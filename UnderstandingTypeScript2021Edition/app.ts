1e05574ba2616a9223d8d6aadc4ae71fb9c6689d
Alberto González <algonzalez@foundationmedicine.com>
2021-10-05 13:25:38 -0500

    Combine parameters were added to the combine function.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -1,17 +1,17 @@
-enum Role { ADMIN, READ_ONLY, AUTHOR}
-
-const person= {
-  name: 'Maximilian',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 == 'number') {
+    result = input1 + input2
+  }
+  else {
+    result = input1.toString() + input2.toString()
+  }
+  return result;
 }
 
-let favoriteActivies: string[];
-favoriteActivies = ['Sports']
+const combineAges = combine(30, 26)
+console.log(combineAges);
 
-console.log(person.name);
 
-for(const hobby of person.hobbies){
-  console.log(hobby.toUpperCase());
-}
+const combineNames = combine('Max', 'Anna')
+console.log(combineNames);
