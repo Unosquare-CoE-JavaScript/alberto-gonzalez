86a433cf87dfcde809d88e57b6488d82812eb274
Alberto González <algonzalez@foundationmedicine.com>
2021-10-06 11:51:37 -0500

    Geters and setters were added.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -30,17 +30,34 @@ class ITDepartment extends Department {
   }
 }
 class AccoutingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found.')
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error('Please pass in a valid value!')
+    }
+    this.addReport(value)
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting')
+    this.lastReport = reports[0]
   }
 
   addReport(text: string) {
     this.reports.push(text)
+    this.lastReport = text
   }
 
   printReports() {
     console.log(this.reports);
-
   }
 
   addEmployee(name: string) {
