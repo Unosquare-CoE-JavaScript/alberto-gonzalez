fab1c2bb68b15b9468285b947ee353113f943558
Alberto González <algonzalez@foundationmedicine.com>
2021-10-06 11:14:13 -0500

    A short cut to define the properties in the constructor function.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -1,13 +1,16 @@
 class Department {
-  name: string;
+
+  // private id: string;
+  // private name: string;
   private employees: string[] = []
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private id: string, public name: string) {
+    // this.name = n;
+    // this.id = id;
   }
 
   describe(this: Department) {
-    console.log('Department: ' + this.name);
+    console.log(`Department: (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -21,7 +24,7 @@ class Department {
 
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department('d1', 'Accounting');
 accounting.describe()
 accounting.addEmployee('Max')
 accounting.addEmployee('Manu')
