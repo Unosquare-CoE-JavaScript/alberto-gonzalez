6fb14831920f3832b546d4efeba45d7e66952e21
Alberto González <algonzalez@foundationmedicine.com>
2021-10-06 16:09:17 -0500

    A type guard was added with 'typeof'.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -5,7 +5,7 @@ type Admin = {
 
 type Employee = {
   name: string;
-  starDate: Date;
+  startDate: Date;
 }
 
 type ElevatedEmployee = Admin & Employee;
@@ -13,10 +13,59 @@ type ElevatedEmployee = Admin & Employee;
 const e1: ElevatedEmployee = {
   name: 'Max',
   privileges: ['create-server'],
-  starDate: new Date()
+  startDate: new Date()
 }
 
 type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+type UnknowEmployee = Employee | Admin;
+function printEployeeInformation(emp: UnknowEmployee) {
+  console.log('Name: ' + emp.name);
+  if ('privileges' in emp) {
+    console.log('Privililegs: ' + emp.privileges);
+
+  }
+  if ('startDate' in emp) {
+    console.log('Start Date: ' + emp.startDate);
+
+  }
+
+}
+
+printEployeeInformation({ name: 'Manu', startDate: new Date() })
+
+class Car {
+  drive() {
+    console.log('Driving');
+
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck.');
+
+  }
+  loadCargo(amount: number) {
+    console.log('Loading cargo...');
+
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(10)
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2)
