0b66c1d502b9fab560127449a1486601574faca3
Alberto González <algonzalez@foundationmedicine.com>
2021-10-07 16:48:39 -0500

    An autobinding decorator was created.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -93,3 +93,29 @@ class Product {
   }
 
 }
+
+function Autobind(target: any, methodName: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  }
+  return adjDescriptor;
+}
+
+class Printer {
+  message = 'This workds!'
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+const button = document.querySelector('button')!
+button.addEventListener('click', p.showMessage)
