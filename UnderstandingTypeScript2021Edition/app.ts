8b39798acd7983163f94538e6aa5b81db083b6ea
Alberto González <algonzalez@foundationmedicine.com>
2021-10-07 14:28:22 -0500

    A decorator was added to the property title in the Product class.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -1,6 +1,6 @@
 function Logger(logString: string) {
   console.log('LOGGER FACTORY');
-  
+
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
@@ -34,3 +34,32 @@ class Person {
 
 const pers = new Person();
 console.log(pers);
+
+function Log(target: any, propertyName: string | symbol) {
+  console.log('Property decorator!');
+  console.log(target, propertyName);
+}
+class Product {
+  @Log
+  title: string;
+  _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    }
+    else {
+      throw new Error('Invalid price.')
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+
+}
