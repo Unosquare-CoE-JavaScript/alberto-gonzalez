3f94c2aff6d7dbad9df1a78ec0fdf524f198e4d3
Alberto González <algonzalez@foundationmedicine.com>
2021-10-07 10:34:13 -0500

    Another generic example was added.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -4,3 +4,19 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: 'Max', hobbies: ['Swwim'] }, { age: 30 })
 console.log();
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length == 0) {
+    descriptionText = 'Got 1 element'
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.'
+  }
+  return [element, descriptionText]
+}
+
+console.log(countAndDescribe('Hi there!'));
