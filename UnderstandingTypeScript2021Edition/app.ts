39346a156f45539f744fe337eb3c475034ecdeb0
Alberto González <algonzalez@foundationmedicine.com>
2021-10-07 10:26:58 -0500

    A constrain example was added.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -1,5 +1,5 @@
-function merge<T, U>(objA: T, objB: U) {
-  return Object.assign(objA, objB)
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
 
 const mergedObj = merge({ name: 'Max', hobbies: ['Swwim'] }, { age: 30 })
