6b78ea80774594ae24e7cab7913bc0c69c65c3cc
Alberto González <algonzalez@foundationmedicine.com>
2021-10-08 13:08:53 -0500

    The class: 'More classes && custom Types' was implemented.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -1,6 +1,22 @@
+enum ProjectStatus {
+  Active, Finished
+}
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) { }
+}
+
+type Listener = (items: Project[]) => void
+
 class ProjectState {
-  private listeners: any[] = []
-  private projects: any[] = []
+  private listeners: Listener[] = []
+  private projects: Project[] = []
   private static instance: ProjectState;
 
   private constructor() {
@@ -17,18 +33,20 @@ class ProjectState {
     }
   }
 
-  addListener(listenerFn: Function) {
+  addListener(listenerFn: Listener) {
     this.listeners.push(listenerFn)
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
 
-    const newProject = {
-      id: Math.random().toString(),
-      title: title,
-      description: description,
-      people: numOfPeople
-    }
+    const newProject = new Project(
+      Math.random().toString(),
+      title,
+      description,
+      numOfPeople,
+      ProjectStatus.Active
+    )
+
     this.projects.push(newProject)
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice())
@@ -86,7 +104,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedProjects: any[]
+  assignedProjects: Project[]
 
   constructor(private type: 'active' | 'finished',) {
     this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
@@ -98,7 +116,7 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener(
-      (projects: any[]) => {
+      (projects: Project[]) => {
         this.assignedProjects = projects
         this.renderProjects()
       }
