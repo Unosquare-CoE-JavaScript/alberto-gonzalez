e2668b7aed53b367bc536f0748c55b4738244b52
Alberto González <algonzalez@foundationmedicine.com>
2021-10-08 13:17:23 -0500

    The class: 'Filtering projects with Enums' was implemented.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -117,7 +117,13 @@ class ProjectList {
 
     projectState.addListener(
       (projects: Project[]) => {
-        this.assignedProjects = projects
+        const relevantProjects = projects.filter(prj => {
+          if (this.type == 'active') {
+            return prj.status == ProjectStatus.Active
+          }
+          return prj.status == ProjectStatus.Finished
+        })
+        this.assignedProjects = relevantProjects
         this.renderProjects()
       }
     )
@@ -128,6 +134,7 @@ class ProjectList {
 
   private renderProjects() {
     const listEl = document.getElementById(`${this.type}-project-list`)! as HTMLUListElement
+    listEl.innerHTML = ''
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li')
       listItem.textContent = prjItem.title;
