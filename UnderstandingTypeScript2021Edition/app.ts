c6107933fe16520c16494b4f3c85b123263f7503
Alberto González <algonzalez@foundationmedicine.com>
2021-10-11 11:37:53 -0500

    The class: 'Adding a Droppable Area' was implemented.

--- UnderstandingTypeScript2021Edition/app.ts
+++ UnderstandingTypeScript2021Edition/app.ts
@@ -166,8 +166,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements 
 
   @autobind
   dragStartHandler(event: DragEvent): void {
-    console.log(event);
-
+    event.dataTransfer!.setData('text/plain', this.project.id)
+    event.dataTransfer!.effectAllowed = 'move'
   }
 
   dragEndHandler(event: DragEvent): void {
@@ -206,14 +206,16 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
   }
 
   dropHandler(event: DragEvent): void {
-
-
+    console.log(event.dataTransfer!.getData('text/plain'));
   }
 
   @autobind
   dragLeaveHandler(event: DragEvent): void {
-    const listEl = this.element.querySelector('ul')!
-    listEl.classList.remove('droppable')
+    if (event.dataTransfer && event.dataTransfer.types[0] == 'text/plain') {
+      event.preventDefault()
+      const listEl = this.element.querySelector('ul')!
+      listEl.classList.remove('droppable')
+    }
   }
 
   configure() {
